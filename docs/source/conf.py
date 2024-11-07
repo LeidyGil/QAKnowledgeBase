17a15387676b5b92d25141b5620d2be193bbc228
Leidy Gil <leidygilcha@gmail.com>
2024-11-06 19:05:11 -0500

    Update conf.py
    Adding project info.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'QAKnowledgeBase'
+copyright = '2024, LeidyGil'
+author = 'LeidyGil'
 
 release = '0.1'
 version = '0.1.0'
